8a625fd271760c5cbbb49ad96baedd4c1780d386
Rikudawn <180044602+RikuDawn14@users.noreply.github.com>
2025-10-13 15:14:26 -0700

    Update un_install.sh

--- shell_scripts/un_install.sh
+++ shell_scripts/un_install.sh
@@ -4,8 +4,7 @@ sudo pacman -Rsn micro firedragon-catppuccin kate
 
 sudo pacman -S firefox steam discord visual-studio-code-bin obsidian makemkv handbrake qbittorrnet vim
 
-wget -P /usr/share/wallpapers/garuda-mokka https://github.com/RikuDawn14/Garuda_Settings/blob/main/Images/Wallpapers/wallpaper.png
-
-wget -P /usr/share/wallpapers/garuda-mokka https://github.com/RikuDawn14/Garuda_Settings/blob/main/Images/Wallpapers/Ultrawide_wallpaper.png
-
-wget -P /home/rikudawn https://github.com/RikuDawn14/Garuda_Settings/blob/main/dot_files/.vimrc
+# To use these wget commands the repository needs to be public.
+#wget -P /usr/share/wallpapers/garuda-mokka https://github.com/RikuDawn14/Garuda_Settings/blob/main/Images/Wallpapers/wallpaper.png
+#wget -P /usr/share/wallpapers/garuda-mokka https://github.com/RikuDawn14/Garuda_Settings/blob/main/Images/Wallpapers/Ultrawide_wallpaper.png
+#wget -P /home/rikudawn https://github.com/RikuDawn14/Garuda_Settings/blob/main/dot_files/.vimrc
