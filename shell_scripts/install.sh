ee2436d8d9e87f3002347fec944bd3c0be9bc195
Rikudawn <180044602+RikuDawn14@users.noreply.github.com>
2025-11-18 16:36:05 -0800

    Update install.sh

--- shell_scripts/install.sh
+++ shell_scripts/install.sh
@@ -4,7 +4,5 @@ sudo pacman -Rsn micro firedragon-catppuccin kate
 
 sudo pacman -S firefox steam discord vscodium-bin obsidian makemkv handbrake qbittorrnet vim
 
-# To use these wget commands the repository needs to be public.
-wget -P /usr/share/wallpapers/garuda-mokka https://github.com/RikuDawn14/Garuda_Settings/blob/main/Images/Wallpapers/wallpaper.png
-wget -P /usr/share/wallpapers/garuda-mokka https://github.com/RikuDawn14/Garuda_Settings/blob/main/Images/Wallpapers/Ultrawide_wallpaper.png
-wget -P /home/rikudawn https://github.com/RikuDawn14/Garuda_Settings/blob/main/dot_files
+sudo mv ./Images/Wallpapers/wallpaper.png /usr/share/wallpapers/garuda-mokka/wallpaper.png
+sudo mv ./Images/Wallpapers/Ultrawide_wallpaper.png /usr/share/wallpapers/garuda-mokka/Ultrawide_wallpaper.png
